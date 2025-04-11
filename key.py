cb6c3e7147b33e41b97b638cb5e77cd6e089d4d8
Alex <143834474+aplotlib@users.noreply.github.com>
2025-04-11 15:58:06 -0400

    Delete key.py

--- key.py
+++
@@ -1,4 +0,0 @@
-# This file contains the API key for the OpenAI API
-
-# API key for OpenAI
-api_key = "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"
